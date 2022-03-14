7e23af6792e30a50e4334fbf397fa7b8b8d50d7b
Sinai Alfaro <97054941+SinaiAlH@users.noreply.github.com>
2022-03-13 22:21:30 -0600

    changes

--- js/app.js
+++ js/app.js
@@ -23,6 +23,7 @@
  * 
 */
 
+let sections = [];
 
 /**
  * End Global Variables
@@ -39,12 +40,36 @@
 */
 
 // build the nav
+const buildNav = () => {
+    const menu = document.getElementById("navbar__list");
+    let options = "";
 
+    // Build submenu options
+    sections.forEach(function(section) {
+        const text = section.querySelectorAll("h2").length > 0 ? section.querySelectorAll("h2")[0].outerText : `Section ${i}`;
+        const id = section.id;
+        options += `<li class="${id}"><a data-nav="${id}" class="menu__link" href="#${id}"">${text}</a></li>`;
+    });
+    
+    // Add options to nav element on dom
+    menu.innerHTML = options;
+};
 
 // Add class 'active' to section when near top of viewport
-
+//const setActiveSection 
 
 // Scroll to anchor ID using scrollTO event
+const scrollToSection = (evt) => {
+    evt.preventDefault();
+    const element = document.getElementById(evt.currentTarget.getAttribute("data-nav"));
+
+    //scroll to the section
+    element.scrollIntoView( { behavior: "smooth", block: "start", inline: "center" });
+
+    // Update top nav
+    document.querySelectorAll('section').forEach(n => n.classList.remove('your-active-class'));
+    element.classList.add("your-active-class");
+};
 
 
 /**
@@ -56,65 +81,35 @@
 // Build menu 
 window.addEventListener('load', function () {
     // Get sections 
-    const sections = document.querySelectorAll("section");
-    const menu = document.getElementById("navbar__list");
-    let navLi = [];
-    let options = "";
-
-    // Build submenu options
-    for (let i = 0; i < sections.length; i++) {
-        const text = sections[i].querySelectorAll("h2").length > 0 ? sections[i].querySelectorAll("h2")[0].outerText : `Section ${i}`;
-        const id = sections[i].id;
-        options += `<li class="${id}"><a data-nav="${id}" class="menu__link" href="#${id}"">${text}</a></li>`;
-        
-    }
-    // Add options to nav element on dom
-    menu.innerHTML = options;
-    // Scroll to section on link click
-    var menuOptions = document.querySelectorAll(".menu__link");
-    for (let i = 0; i < menuOptions.length; i++) {
-        menuOptions[i].addEventListener("click", function(){
-            document.querySelectorAll('section').forEach(n => n.classList.remove('your-active-class'));
-            document.getElementById(this.getAttribute("data-nav")).classList.add("your-active-class");
-        });        
-    }
+    sections = document.querySelectorAll("section");
     
+    // Build top nav
+    buildNav();
 
-    navLi = document.querySelectorAll("nav ul li");
-    // Set sections as active
+    // Add click behavior to scroll to section on link click
+    document.querySelectorAll(".menu__link").forEach( (opt) =>  opt.addEventListener("click", scrollToSection));    
 
     // Add scroll listener in order to get the current position of the object 
     // and the position of the window to set the section as active
     window.onscroll = () => {
-    var current = "";
-
-    sections.forEach((section) => {
-        const sectionHeight = section.offsetHeight;
-        const sectionTop = (section.getBoundingClientRect().top + window.pageYOffset) - 40;
-        
-            if (
-                scrollY > sectionTop &&
-                scrollY <= sectionTop + sectionHeight
-              ) {
-            current = section.getAttribute("id"); 
-        }
-    });
-
-    // Change the menu list option as active when the displayed position is the option selected.
-    navLi.forEach((li) => {
-        li.classList.remove("active");
-        if (li.classList.contains(current)) {
-            li.classList.add("active");
-        }
+        let navLi = document.querySelectorAll("nav ul li");
+        let current = "";
+    
+        sections.forEach((section) => {
+            const sectionHeight = section.offsetHeight;
+            const sectionTop = (section.getBoundingClientRect().top + window.pageYOffset) - 40;
+            
+            if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+                current = section.getAttribute("id"); 
+            }
         });
+    
+        // Change the menu list option as active when the displayed position is the option selected.
+        navLi.forEach((li) => {
+            li.classList.remove("active");
+            if (li.classList.contains(current)) {
+                li.classList.add("active");
+            }
+            });
     };
-
-
-
-
-
-
-  }, false);
-
-
-
+}, false);
